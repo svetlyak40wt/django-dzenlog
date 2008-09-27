f482fe4866ce1fa03c21c3cfe3611ec1a26a9aa3
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-09-28 02:22:03 +0400

    Simple model for GeneralPost.

--- django_dzenlog/models.py
+++ django_dzenlog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class GeneralPost(models.Model):
+    title = models.CharField('Title', max_length=100)
+    publish_on = models.DateTimeField('Date', blank=True, null=True)
+
+    def __unicode__(self):
+        return 'GeneralPost: %s' % self.title
