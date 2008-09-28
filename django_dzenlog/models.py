e39e0632b7b635f83164f914dcbf5979b9ef78d0
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-09-28 05:24:53 +0400

    Few new fields where added to GeneralPost.

--- django_dzenlog/models.py
+++ django_dzenlog/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+try:
+    from tagging.fields import TagField
+except ImportError:
+    TagField = None
 
 class GeneralPost(models.Model):
-    title = models.CharField('Title', max_length=100)
-    publish_on = models.DateTimeField('Date', blank=True, null=True)
+    title       = models.CharField(_('Title'), max_length=100)
+    slug        = models.SlugField(_('Slug title'), max_length=100, unique=True)
+    created_on  = models.DateTimeField(_('Create date'), blank=True)
+    updated_on  = models.DateTimeField(_('Update date'), blank=True)
+    publish_on  = models.DateTimeField(_('Publish date'), blank=True, null=True)
+    comments_on = models.BooleanField(_('Comments On'), default=True)
+
+    if TagField:
+        tags    = TagField()
 
     def __unicode__(self):
-        return 'GeneralPost: %s' % self.title
+        return self.title
