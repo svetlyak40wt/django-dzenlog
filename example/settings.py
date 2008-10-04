9f659a3f914a6440529f05802d63e9532d37a271
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-10-04 05:50:43 +0400

    Ability to load local_settings.py.

--- example/settings.py
+++ example/settings.py
@@ -1,7 +1,7 @@
 import deseb
 # Django settings for example project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -10,9 +10,9 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'dzenlog'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
+DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
@@ -87,3 +87,7 @@ INSTALLED_APPS = (
     'django_extensions',
 )
 
+try:
+    from local_settings import *
+except ImportError:
+    pass
