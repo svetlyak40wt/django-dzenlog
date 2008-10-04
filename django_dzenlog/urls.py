931e859187d04076206500ac4ed3f545704a4af5
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-10-04 06:12:28 +0400

    Template for posts list.

--- django_dzenlog/urls.py
+++ django_dzenlog/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls.defaults import *
 from models import GeneralPost
 
-info_dict = {
+post_list = {
     'queryset': GeneralPost.objects.all(),
-    'slug_field': 'hash'
 }
 
 urlpatterns = patterns('django.views.generic',
-   (r'^(?P<slug>[a-z0-9-])/$', 'list_detail.object_detail', info_dict),
-   (r'^/$', 'list_detail.object_list', info_dict),
+   (r'^(?P<slug>[a-z0-9-])/$', 'list_detail.object_detail', post_list),
+   (r'^$', 'list_detail.object_list', post_list),
 )
 
