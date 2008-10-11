9980c28a5fc2c40e4c2f533cae794f35cf0b217e
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-10-11 20:54:32 +0400

    Only SQL panel for debug toolbar.

--- example/settings.py
+++ example/settings.py
@@ -90,6 +90,18 @@ INSTALLED_APPS = (
     'django_extensions',
 )
 
+DEBUG_TOOLBAR_PANELS = (
+#    'debug_toolbar.panels.version.VersionDebugPanel',
+#    'debug_toolbar.panels.timer.TimerDebugPanel',
+#    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+#    'debug_toolbar.panels.headers.HeaderDebugPanel',
+#    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+#    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+#    'debug_toolbar.panels.cache.CacheDebugPanel',
+#    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 try:
     from local_settings import *
 except ImportError:
