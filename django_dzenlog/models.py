1c04f037548c39346008438e5d06f517552cc3fc
Alexander Artemenko <svetlyak.40wt@gmail.com>
2008-12-05 14:14:42 +0300

    Some fixes for pylint pleasure.

--- django_dzenlog/models.py
+++ django_dzenlog/models.py
@@ -1,10 +1,14 @@
+'''
+Inherit your own models from GeneralPost class
+and define there application specific fieldd.
+'''
+
 import logging
 from datetime import datetime
 
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from django.template.loader import render_to_string
 from django.core.urlresolvers import reverse, NoReverseMatch
 
 import settings
@@ -16,12 +20,14 @@ if settings.HAS_TAGGING:
     from tagging.models import Tag
 
 
-from utils import upcast, virtual
+from utils import upcast
 
 def published(queryset):
+    'Filter out not published posts.'
     return queryset.filter(publish_at__lte=datetime.now())
 
 class GeneralPost(models.Model):
+    '''Base class for blog posts.'''
     # templates
     list_template             = 'django_dzenlog/list.html'
     detail_template           = 'django_dzenlog/detail.html'
@@ -35,8 +41,10 @@ class GeneralPost(models.Model):
     author      = models.ForeignKey(User)
     title       = models.CharField(_('Title'), max_length=100)
     slug        = models.SlugField(_('Slug title'), max_length=100, unique=True)
-    created_at  = models.DateTimeField(_('Create date'), blank=True, editable=False)
-    updated_at  = models.DateTimeField(_('Update date'), blank=True, editable=False)
+    created_at  = models.DateTimeField(_('Create date'),
+                                       blank=True, editable=False)
+    updated_at  = models.DateTimeField(_('Update date'),
+                                       blank=True, editable=False)
     publish_at  = models.DateTimeField(_('Publish date'), blank=True, null=True)
     comments_on = models.BooleanField(_('Comments On'), default=True)
 
@@ -44,6 +52,9 @@ class GeneralPost(models.Model):
         tags    = TagField()
 
         def _save_tags(self, *args, **kwargs):
+            '''Helper method which makes sure,
+               that tags are binded to GeneralPost
+               content type.'''
             instance = self.downcast()
             setattr(instance, '_tags_cache', self._tags_cache)
             self._meta.get_field('tags')._save(instance=instance)
@@ -53,7 +64,9 @@ class GeneralPost(models.Model):
     else:
         def __init__(self, *args, **kwargs):
             if kwargs.pop('tags', None) is not None:
-                logging.getLogger('django_dzenlog').warning('Tagging is not supported, add "tagging" into the INSTALLED_APPS.')
+                logging.getLogger('django_dzenlog').warning(
+                    'Tagging is not supported, '
+                    'add "tagging" into the INSTALLED_APPS.')
             super(GeneralPost, self).__init__(*args, **kwargs)
 
     def __unicode__(self):
@@ -68,7 +81,11 @@ class GeneralPost(models.Model):
         ordering = ('-publish_at',)
 
     def get_edit_url(self):
-        return reverse('admin-root', args = ['/'.join((self._meta.app_label, self._meta.module_name, unicode(self.id)))])
+        return reverse('admin-root',
+                        args = ['/'.join((
+                            self._meta.app_label,
+                            self._meta.module_name,
+                            unicode(self.id)))])
 
     def upcast(self):
         return upcast(self)
@@ -76,9 +93,12 @@ class GeneralPost(models.Model):
     def get_absolute_url(self):
         try:
             obj = self.upcast()
-            return reverse('dzenlog-%s-details' % obj._meta.module_name, kwargs=dict(slug=self.slug))
+            return reverse('dzenlog-%s-details' % obj._meta.module_name,
+                            kwargs=dict(slug=self.slug))
         except NoReverseMatch:
-            return reverse('dzenlog-%s-details' % self.downcast()._meta.module_name, kwargs=dict(slug=self.slug))
+            return reverse('dzenlog-%s-details' % \
+                                self.downcast()._meta.module_name,
+                            kwargs=dict(slug=self.slug))
 
     def save(self):
         today = datetime.today()
